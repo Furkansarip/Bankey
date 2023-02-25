caee2ee14c29c387d4e4eaf743ffcd39d0b59914
Furkan Sarı <furkansarip@gmail.com>
2023-02-25 11:38:34 +0300

    Adding the login button

--- Bankey/Bankey/LoginViewController.swift
+++ Bankey/Bankey/LoginViewController.swift
@@ -10,10 +10,12 @@ import UIKit
 class LoginViewController: UIViewController {
 
     var loginView = LoginView()
+    let loginButton = UIButton(type: .system)
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         view.addSubview(loginView)
+        view.addSubview(loginButton)
         style()
         layout()
     }
@@ -24,14 +26,25 @@ class LoginViewController: UIViewController {
 extension LoginViewController {
     private func style() {
         loginView.translatesAutoresizingMaskIntoConstraints = false
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        loginButton.configuration = .filled()
+        loginButton.setTitle("Sign In", for: [])
+        loginButton.addTarget(self, action: #selector(loginTapped), for: .primaryActionTriggered)
         
     }
     private func layout() {
         NSLayoutConstraint.activate([
             loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
-            view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1)
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1),
+            loginButton.topAnchor.constraint(equalToSystemSpacingBelow: loginView.bottomAnchor, multiplier: 2),
+            loginButton.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginButton.trailingAnchor, multiplier: 1)
         ])
+    }
+    
+    @objc func loginTapped() {
+        
     }
 }
 
