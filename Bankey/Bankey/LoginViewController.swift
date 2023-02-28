278ea52e3c60e53d7ed4d2f46fb315531601f6bf
Furkan Sarı <furkansarip@gmail.com>
2023-02-28 14:15:07 +0300

    Sign in button shake animation & Constraints refactor

--- Bankey/Bankey/LoginViewController.swift
+++ Bankey/Bankey/LoginViewController.swift
@@ -12,12 +12,12 @@ protocol LoginViewControllerProtocol : AnyObject {
 }
 
 class LoginViewController: UIViewController {
-
+    
     var loginView = LoginView()
     let loginButton = UIButton(type: .system)
     let errorLabel = UILabel()
     let titleLabel = UILabel()
-    let descriptionLabel = UILabel()
+    let subtitleLabel = UILabel()
     weak var delegate : LoginViewControllerProtocol?
     var username : String? {
         return loginView.usernameTextField.text
@@ -25,30 +25,37 @@ class LoginViewController: UIViewController {
     var password : String? {
         return loginView.passwordTextField.text
     }
+    
+    var leadingEdgeOnScreen : CGFloat = 16
+    var leadingEdgeOffScreen: CGFloat = -1000
+    var titleLeadingAnchor : NSLayoutConstraint?
+    var subtitleLeadingAnchor : NSLayoutConstraint?
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         view.addSubview(titleLabel)
-        view.addSubview(descriptionLabel)
+        view.addSubview(subtitleLabel)
         view.addSubview(loginView)
         view.addSubview(loginButton)
         view.addSubview(errorLabel)
         style()
         layout()
     }
-
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        animate()
+    }
+    
+    
 }
 
 extension LoginViewController  {
-    
-    
     private func style() {
         loginView.translatesAutoresizingMaskIntoConstraints = false
         loginButton.translatesAutoresizingMaskIntoConstraints = false
         errorLabel.translatesAutoresizingMaskIntoConstraints = false
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
         
         errorLabel.text = "Username / Password cannot be blank"
         errorLabel.textAlignment = .center
@@ -66,50 +73,94 @@ extension LoginViewController  {
         titleLabel.adjustsFontForContentSizeCategory = true
         
         
-        descriptionLabel.text = "Your premium source for all things banking!"
-        descriptionLabel.numberOfLines = 0
-        descriptionLabel.adjustsFontForContentSizeCategory = true
-        descriptionLabel.textAlignment = .center
-        descriptionLabel.font = .preferredFont(forTextStyle: .title3)
-    
+        subtitleLabel.text = "Your premium source for all things banking!"
+        subtitleLabel.numberOfLines = 0
+        subtitleLabel.adjustsFontForContentSizeCategory = true
+        subtitleLabel.textAlignment = .center
+        subtitleLabel.font = .preferredFont(forTextStyle: .title3)
+        
         
     }
     private func layout() {
         NSLayoutConstraint.activate([
-            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            
-            descriptionLabel.topAnchor.constraint(equalToSystemSpacingBelow: titleLabel.bottomAnchor, multiplier: 3),
-            descriptionLabel.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
-            view.trailingAnchor.constraint(equalToSystemSpacingAfter: descriptionLabel.trailingAnchor, multiplier: 1),
-            
-            loginView.topAnchor.constraint(equalToSystemSpacingBelow: descriptionLabel.bottomAnchor, multiplier: 2),
-            loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
-            view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1),
-                
-            loginButton.topAnchor.constraint(equalToSystemSpacingBelow: loginView.bottomAnchor, multiplier: 2),
-            loginButton.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
-            view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginButton.trailingAnchor, multiplier: 1),
-            
-            errorLabel.topAnchor.constraint(equalToSystemSpacingBelow: loginButton.bottomAnchor, multiplier: 2),
-            errorLabel.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
-            view.trailingAnchor.constraint(equalToSystemSpacingAfter: errorLabel.trailingAnchor, multiplier: 1)
+            subtitleLabel.topAnchor.constraint(equalToSystemSpacingBelow: titleLabel.bottomAnchor, multiplier: 3),
+            titleLabel.trailingAnchor.constraint(equalTo: loginView.trailingAnchor)
         ])
+            
+        titleLeadingAnchor = titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: leadingEdgeOffScreen)
+        titleLeadingAnchor?.isActive = true
+        
+        // Subtitle
+        NSLayoutConstraint.activate([
+            loginView.topAnchor.constraint(equalToSystemSpacingBelow: subtitleLabel.bottomAnchor, multiplier: 3),
+            subtitleLabel.trailingAnchor.constraint(equalTo: loginView.trailingAnchor)
+        ])
+            
+        subtitleLeadingAnchor = subtitleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: leadingEdgeOffScreen)
+        subtitleLeadingAnchor?.isActive = true
+        
+        // LoginView
+        NSLayoutConstraint.activate([
+            loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 2),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 2),
+            view.centerYAnchor.constraint(equalTo: loginView.centerYAnchor),
+        ])
+        
+        // Button
+        NSLayoutConstraint.activate([
+            loginButton.topAnchor.constraint(equalToSystemSpacingBelow: loginView.bottomAnchor, multiplier: 2),
+            loginButton.leadingAnchor.constraint(equalTo: loginView.leadingAnchor),
+            loginButton.trailingAnchor.constraint(equalTo: loginView.trailingAnchor),
+        ])
+        
+        // Error message
+        NSLayoutConstraint.activate([
+            errorLabel.topAnchor.constraint(equalToSystemSpacingBelow: loginButton.bottomAnchor, multiplier: 2),
+            errorLabel.leadingAnchor.constraint(equalTo: loginView.leadingAnchor),
+            errorLabel.trailingAnchor.constraint(equalTo: loginView.trailingAnchor)
+        ])
+        
     }
     
+}
+extension LoginViewController {
     @objc func loginTapped() {
         guard let username = username,let password = password else {
             assertionFailure("Cannot be blank")
+            
             return
         }
-
+        
         if username.isEmpty || password.isEmpty {
             errorLabel.isHidden = false
+            shakeButton()
         } else {
             loginButton.configuration?.showsActivityIndicator = true
             delegate?.didLogin()
         }
     }
+}
+
+
+extension LoginViewController {
+    private func animate() {
+        let animator1 = UIViewPropertyAnimator(duration: 1, curve: .easeInOut) {
+            self.titleLeadingAnchor?.constant = self.leadingEdgeOnScreen
+            self.subtitleLeadingAnchor?.constant = self.leadingEdgeOnScreen
+            self.view.layoutIfNeeded()
+        }
+        animator1.startAnimation()
+    }
     
+    private func shakeButton() {
+        let animation = CAKeyframeAnimation()
+        animation.keyPath = "position.x"
+        animation.values = [0, 10, -10, 10, 0]
+        animation.keyTimes = [0, 0.16, 0.5, 0.83, 1]
+        animation.duration = 0.4
+
+        animation.isAdditive = true
+        loginButton.layer.add(animation, forKey: "shake")
+    }
 }
 
